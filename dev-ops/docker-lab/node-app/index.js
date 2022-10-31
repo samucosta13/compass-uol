16efe6d899d69717e56d7422f28b320698b49253
Samuel <samuelcosta.dev@gmail.com>
2022-10-31 14:42:09 -0300

    Node app - ultimate update

--- dev-ops/docker-lab/node-app/index.js
+++ dev-ops/docker-lab/node-app/index.js
@@ -3,16 +3,16 @@ const redis = require('redis')
 
 const app = express()
 const client = redis.createClient({
-    host: 'redis-server',
+    host: 'redis-server', 
     port: 6379
 })
 
 client.set('visits',0);
 
-app.get('/',(req, res)=>{
-    client.get('visits',(err, visits)=>{
+app.get('/', (req, res)=>{
+    client.get('visits', (err, visits)=>{
         visits = parseInt(visits) + 1
-        res.send('Number of visits is: '+visits)
+        res.send('Number of visits is:' + visits)
         client.set('visits', parseInt(visits))
     })
 })
