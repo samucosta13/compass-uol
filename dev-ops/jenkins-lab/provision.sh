eb25911786049ac22ecb7cb04e64bac7a888a1ec
Samuel <samuelcosta.dev@gmail.com>
2022-11-02 15:24:46 -0300

    Jenkins Lab

---
+++ dev-ops/jenkins-lab/provision.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+## Instalação do Jenkins
+yum install -y epel-release
+yum install -y wget
+
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install -y java-11-openjdk
+yum install jenkins -y
+systemctl daemon-reload
+
+## Instalação do Docker e Docker Compose
+sudo yum install -y yum-utils
+sudo yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl daemon-reload
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl restart docker
